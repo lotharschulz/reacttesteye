22a348dde0689284c0f1d1820fb99919f4aa22a5
Lothar Schulz <lothar.sch@gmx.de>
2016-02-08 00:07:00 +0100

    flickr api url comment

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=[api_key]&photoset_id=[photoset_id]&user_id=[user_id]&format=json&per_page=21&page=1&extras=url_o,url_m,url_l,url_c
 import React from 'react';
 
 class App extends React.Component {
