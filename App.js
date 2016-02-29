d62928e89d4ac4898ccfdf4720fea95167fcc743
Lothar Schulz <lothar.sch@gmx.de>
2016-02-29 23:50:01 +0100

    neptunian ref added

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=[api_key]&photoset_id=[photoset_id]&user_id=[user_id]&format=json&per_page=21&page=1&extras=url_o,url_m,url_l,url_c
+// https://github.com/neptunian/react-photo-gallery
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from 'react-photo-gallery';
